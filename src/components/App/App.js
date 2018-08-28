6e783365059fcbadc934951e99601313cc4f8d2f
Mihail Gumennii <gumennii@Mihails-MacBook-Pro.local>
2018-08-28 14:32:01 -0700

    add react hot loader to hot load statefull components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
 
-const App = () => {
-  return (
-    <div>
-      Application!!
-    </div>
-  )
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        Application!!!
+        <div>
+          <button onClick={this.increment.bind(this)}>Increment</button>
+          Counter: {this.state.count}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default hot(module)(App)
